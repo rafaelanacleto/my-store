9d374f09e992e48589226230e60cd135cdcb44d1
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2020-06-01 18:10:45 -0300

    criado o top-bar e product list

--- my-store/src/app/app.module.ts
+++ my-store/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ProductListComponent,
+      TopBarComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ my-store/src/app/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+    {
+        name: 'Iphone XR',
+        price: 799,
+        description: 'A Large iphone, national first description'
+    },
+    {
+        name: 'Moto G8 Power',
+        price: 299,
+        description: 'Primeiro cellular novo comprado dia 20/04'
+    },
+    {
+        name: 'Samsung A12',
+        price: 788,
+        description: ''
+    },
+    {
+        name: 'Xiaomi',
+        price: 233,
+        description: 'A xiaomi mobile china'
+    }
+];
